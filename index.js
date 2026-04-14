ec8f699270d46d424ea82e0aa3e0c60a3b92bb1e
Marek Karasz <marekkarasz@googlemail.com>
2026-04-14 11:02:48 +0100

    setting up all random values

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-canvas.style.backgroundColor = "rgb(150, 50, 10)";
+canvas.style.backgroundColor = "rgb(0, 0, 0)";
 
 canvas.addEventListener("click", (e) => {
   circles(e);
@@ -12,8 +12,11 @@ function circles(e) {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
+  const radius = 2 + Math.random() * 40;
+  const x = radius + Math.random() * (innerWidth - radius * 2);
+  const y = radius + Math.random() * (innerHeight - radius * 2);
   ctx.beginPath();
-  ctx.arc(e.x, e.y, 30, 0, Math.PI * 2);
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
   ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
   ctx.fill();
 }
